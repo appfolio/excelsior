e7f2e84267a666d5b9e1b61d8536635be08d77a3
Jingyu <jingyu.zhang@appfolio.com>
2019-04-05 15:30:44 -0700

    run rails app:update and manually fix some changes

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,3 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require File.expand_path('../initializers/debug_require', __FILE__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-
-
