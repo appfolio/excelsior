2301096affc4d7246949db564650e92d41396e1a
Magne Land <magneland@users.noreply.github.com>
2022-07-22 13:39:28 -0700

    Revert "Fix CVE 2022 32224"

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Appreciations
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
-
-    # TODO: remove this line and verify nothing breaks
-    config.time_zone = 'Pacific Time (US & Canada)'
-
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Pacific Time (US & Canada)'
   end
 end
 
